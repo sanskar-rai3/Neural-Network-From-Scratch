b29b94e88135909b5f77020618f8fcb64ca93665
Sanskar Rai <sanskarr6812@gmail.com>
2026-08-19 19:59:18 +0545

    added optimizer

---
+++ include/optimizer.h
@@ -0,0 +1,85 @@
+/*
+ * Neural Network From Scratch in C
+ *
+ * Copyright (C) 2026 Sanskar Rai 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __OPTIMIZER_H_
+#define __OPTIMIZER_H_
+
+#include "common.h"
+
+/* Forward declaration to avoid including nn.h here. */
+typedef struct Network Network;
+
+/*==============================================================================
+ * Types
+ *============================================================================*/
+
+/**
+ * @brief Specifies the optimization algorithm used to update parameters.
+ */
+typedef enum OptimizerType {
+    OPT_NULL = 0, /**< No optimization algorithm. */
+    OPT_SGD        /**< Stochastic Gradient Descent. */
+} OptimizerType;
+
+/**
+ * @brief Configuration for a neural network optimizer.
+ */
+typedef struct Optimizer {
+    OptimizerType type; /**< Optimization algorithm to use. */
+    float learning_rate; /**< Step size used for parameter updates. */
+} Optimizer;
+
+/*==============================================================================
+ * Creation & Destruction
+ *============================================================================*/
+
+/**
+ * @brief Initializes an optimizer.
+ *
+ * @param optimizer    Pointer to the Optimizer to initialize.
+ * @param learning_rate Learning rate used during parameter updates.
+ * @param type          Optimization algorithm to use.
+ */
+void optimizer_init(Optimizer *optimizer, float learning_rate, OptimizerType type);
+
+/**
+ * @brief Destroys an optimizer and releases any resources it owns.
+ *
+ * @param optimizer Pointer to the Optimizer to destroy.
+ */
+void optimizer_destroy(Optimizer *optimizer);
+
+/*==============================================================================
+ * Optimization
+ *============================================================================*/
+
+/**
+ * @brief Updates all trainable parameters in the network using their gradients.
+ *
+ * For SGD, parameters are updated according to:
+ *
+ *     W = W - learning_rate * dW
+ *     b = b - learning_rate * db
+ *
+ * @param optimizer Pointer to the initialized Optimizer.
+ * @param network   Pointer to the Network whose parameters will be updated.
+ */
+void optimizer_step(Optimizer *optimizer, Network *network);
+
+#endif
---
+++ src/optimizer.c
@@ -0,0 +1,91 @@
+/*
+ * Neural Network From Scratch in C
+ *
+ * Copyright (C) 2026 Sanskar Rai 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "optimizer.h"
+#include "dense.h"
+#include "nn.h"
+
+#include <assert.h>
+
+/*==============================================================================
+ * Creation & Destruction
+ *============================================================================*/
+
+void optimizer_init(Optimizer *optimizer, float learning_rate, OptimizerType type) {
+    assert(optimizer);
+    assert(learning_rate > 0.0f);
+
+    optimizer->type = type;
+    optimizer->learning_rate = learning_rate;
+}
+
+void optimizer_destroy(Optimizer *optimizer) {
+    assert(optimizer);
+
+    optimizer->type = OPT_NULL;
+    optimizer->learning_rate = 0.0f;
+}
+
+/*==============================================================================
+ * Optimization
+ *============================================================================*/
+
+void optimizer_step(Optimizer *optimizer, Network *network) {
+    assert(optimizer);
+    assert(network);
+
+    switch (optimizer->type) {
+
+        case OPT_SGD:
+            for (usize i = 0; i < network->layer_count; i++) {
+                Dense *dense = &network->layers[i].dense;
+
+                /*
+                 * W = W - learning_rate * dW
+                 */
+                matrix_scalar_multiply_inplace(
+                    &dense->d_weights,
+                    optimizer->learning_rate
+                );
+
+                matrix_subtract_inplace(
+                    &dense->weights,
+                    &dense->d_weights
+                );
+
+                /*
+                 * b = b - learning_rate * db
+                 */
+                matrix_scalar_multiply_inplace(
+                    &dense->d_bias,
+                    optimizer->learning_rate
+                );
+
+                matrix_subtract_inplace(
+                    &dense->bias,
+                    &dense->d_bias
+                );
+            }
+            break;
+
+        case OPT_NULL:
+        default:
+            break;
+    }
+}
