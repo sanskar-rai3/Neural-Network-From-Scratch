7974c0a9a5a0b975c03a56af1d4931c219c8cae1
Sanskar Rai <sanskarr6812@gmail.com>
2026-09-19 14:10:10 +0545

    feat: add tensor creation and destruction

---
+++ include/math/tensor.h
@@ -0,0 +1,38 @@
+/*
+ * Neural Network From Scratch in C
+ *
+ * Copyright (C) 2026 Sanskar Rai 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TENSOR_H
+#define TENSOR_H
+
+#include "common.h"
+
+typedef struct {
+    usize *shape;
+    usize rank;
+
+    usize *strides;
+
+    usize size;
+    float *data;
+} Tensor;
+
+void tensor_create(Tensor *t, usize rank, const usize *shape);
+void tensor_destroy(Tensor *t);
+
+#endif /* TENSOR_H */
---
+++ src/math/tensor.c
@@ -0,0 +1,53 @@
+/*
+ * Neural Network From Scratch in C
+ *
+ * Copyright (C) 2026 Sanskar Rai 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common.h"
+#include "math/tensor.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+void tensor_create(Tensor *t, usize rank, const usize *shape) {
+    t->rank = rank;
+
+    t->shape = malloc(rank * sizeof(usize));
+    memcpy(t->shape, shape, rank * sizeof(usize));
+
+    t->size = 1;
+    for (usize i = 0; i < rank; i++)
+        t->size *= t->shape[i];
+
+    t->strides = malloc(rank * sizeof(usize));
+    t->strides[rank - 1] = 1;
+    for (usize i = rank - 1; i > 0; i--)
+        t->strides[i - 1] = t->strides[i] * t->shape[i];
+
+    t->data = malloc(t->size * sizeof(float));
+}
+
+void tensor_destroy(Tensor *t) {
+    free(t->data);
+    free(t->shape);
+    free(t->strides);
+
+    t->size    = 0;
+    t->rank    = 0;
+    t->shape   = NULL;
+    t->strides = NULL;
+}
